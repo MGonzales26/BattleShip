a172953b39d6d7664c3e1600931cb264088e38ec
Megan Campbell <megan.campbell26@yahoo.com>
2020-10-16 19:25:54 -0600

    Refactor tests to test board creation

--- test/board_test.rb
+++ test/board_test.rb
@@ -13,32 +13,16 @@ class BoardTest < Minitest::Test
   end
 
   def test_it_has_cells
-    skip
     board = Board.new
     
-    # assert_equal {
-    #   "A1" => Cell.new("A1"),
-    #   "A2" => Cell.new("A2"),
-    #   "A3" => Cell.new("A3"),
-    #   "A4" => Cell.new("A4"),
-    #   "B1" => Cell.new("B1"),
-    #   "B2" => Cell.new("B2"),
-    #   "B3" => Cell.new("B3"),
-    #   "B4" => Cell.new("B4"),
-    #   "C1" => Cell.new("C1"),
-    #   "C2" => Cell.new("C2"),
-    #   "C3" => Cell.new("C3"),
-    #   "C4" => Cell.new("C4"),
-    #   "D1" => Cell.new("D1"),
-    #   "D2" => Cell.new("D2"),
-    #   "D3" => Cell.new("D3"),
-    #   "D4" => Cell.new("D4")
-    # }, board.cells
-
-    # assert_equal expected, board.cells
+    assert_equal "A1", board.cells.keys[0]
+    assert_equal "D4", board.cells.keys[-1]
+    assert_instance_of Cell, board.cells.values[0]
+    assert_instance_of Cell, board.cells.values[-1]
   end
 
   def test_valid_coordinate
+    skip
     board = Board.new
 
     assert_equal true, board.valid_coordinate?("A1")
@@ -49,6 +33,7 @@ class BoardTest < Minitest::Test
   end
 
   def test_placement_is_valid
+    skip
     board = Board.new
 
     cruiser = Ship.new("Cruiser", 3)
@@ -59,6 +44,7 @@ class BoardTest < Minitest::Test
   end
 
   def test_placement_is_consecutive
+    skip
     board = Board.new
 
     cruiser = Ship.new("Cruiser", 3)
@@ -68,5 +54,12 @@ class BoardTest < Minitest::Test
     assert_equal false, board.valid_placement?(submarine, ["A1", "C1"])
     assert_equal false, board.valid_placement?(cruiser, ["A3", "A2", "A1"])
     assert_equal false, board.valid_placement?(submarine, ["C1", "B1"])
+  end
+
+  def test_create_board
+    skip
+    board = Board.new
+
+    board.create_board
   end
 end
