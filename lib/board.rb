dfe57227b6eea3c2d2588332276a85d1bd308162
Yesi Meza <yesi.meza10@gmail.com>
2020-10-17 12:28:44 -0600

    Create consecutive numbers method and passes test

--- lib/board.rb
+++ lib/board.rb
@@ -1,17 +1,21 @@
 class Board
-  attr_reader :cells
+  attr_reader :cells,
+              :rows,
+              :columns
 
   def initialize
-    @cells = make_cells
+    @cells   = make_cells
+    @rows    = rows
+    @columns = columns
   end
 
   def create_board
     size = 4
     alphabet = ("A".."Z").to_a
-    rows = (1..size).to_a
-    columns = ("A"..alphabet[size-1]).to_a
-    coords = columns.map do |letter|
-      rows.map do |number|
+    @rows = (1..size).to_a
+    @columns = ("A"..alphabet[size-1]).to_a
+    coords = @columns.map do |letter|
+      @rows.map do |number|
         letter + number.to_s
       end
     end
@@ -25,7 +29,7 @@ class Board
     end
     cells
   end
-  
+
   # require 'pry'; binding.pry
 
   def valid_coordinate?(coord)
@@ -33,21 +37,37 @@ class Board
       cell.coordinate == coord
     end
   end
-  
-  def valid_ship_length(ship, placement)
+
+  def valid_ship_length?(ship, placement)
     ship.length == placement.count
   end
 
-  def numbers_consecutive?(placement)
-    numbers = []
-    placement.each do |place|
-      numbers << place.chars[1].ord
+  def isolate_numbers(placement)
+      placement.map do |place|
+      place[1].to_i
     end
-    # require 'pry'; binding.pry
+  end
+
+  def numbers_consecutive?(placement)
+    # require "pry"; binding.pry
+  something = @rows.each_cons(placement.count).to_a
+  something.any? do |some|
+    isolate_numbers(placement) == some
+    end
+  end
+    # numbers = []
+    # placement.each do |place|
+    #   numbers << place.chars[1].ord
+    # # end
+    # # require 'pry'; binding.pry
     # numbers.each_cons(placement.count).any? do |number|
     # end
     # numbers
-  end
+    def isolate_letters(placement)
+      placement.map do |place|
+        place[0].ord
+      end
+    end
 
   def letters_consecutive?(placement)
     range.to_a.map do |letter|
@@ -55,17 +75,17 @@ class Board
     end.each_cons(3).to_a
   end
 
-  def valid_placement?(ship, placement)
-    valid_ship_length(ship, placement) && (numbers_consecutive?(placement) || letters_consecutive?)
+  # def valid_placement?(ship, placement)
+    # valid_ship_length(ship, placement) && (numbers_consecutive?(placement) || letters_consecutive?)
 
-    # ship.length == placement.count
-    
-    # valid_coordinate
-    # valid_coordinate?
-    # valid_ship_length
-    # numbers consecutive
-    # letters consecutive
-  end
+  #   # ship.length == placement.count
+  #
+  #   # valid_coordinate
+  #   # valid_coordinate?
+  #   # valid_ship_length
+  #   # numbers consecutive
+  #   # letters consecutive
+  # end
 
   # placement.each_cons(ship.length) do |place|
   #   require 'pry'; binding.pry
--- test/board_test.rb
+++ test/board_test.rb
@@ -12,9 +12,10 @@ class BoardTest < Minitest::Test
     assert_instance_of Board, board
   end
 
+
   def test_it_has_cells
     board = Board.new
-    
+
     assert_equal "A1", board.cells.keys[0]
     assert_equal "D4", board.cells.keys[-1]
     assert_instance_of Cell, board.cells.values[0]
@@ -22,7 +23,6 @@ class BoardTest < Minitest::Test
   end
 
   def test_valid_coordinate
-    skip
     board = Board.new
 
     assert_equal true, board.valid_coordinate?("A1")
@@ -32,16 +32,50 @@ class BoardTest < Minitest::Test
     assert_equal false, board.valid_coordinate?("A22")
   end
 
+  def test_it_has_valid_ship_length
+    board = Board.new
+
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    assert_equal false, board.valid_ship_length?(cruiser, ["A1", "A2"])
+    assert_equal false, board.valid_ship_length?(submarine, ["A2", "A3", "A4"])
+  end
+
   def test_placement_is_valid
     skip
     board = Board.new
 
     cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)  
+    submarine = Ship.new("Submarine", 2)
 
     assert_equal false, board.valid_placement?(cruiser, ["A1", "A2"])
     assert_equal false, board.valid_placement?(submarine, ["A2", "A3", "A4"])
   end
+
+  def test_it_can_isolate_numbers
+    board = Board.new
+
+    assert_equal [1, 2, 3], board.isolate_numbers(["A1", "A2", "A3"])
+  end
+
+  def test_numbers_consecutive
+    board = Board.new
+
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    assert_equal true, board.numbers_consecutive?(["A1", "A2", "A3"])
+    assert_equal false, board.numbers_consecutive?(["A1", "A3"])
+    assert_equal true, board.numbers_consecutive?(["A1", "A2"])
+  end
+
+  def test_it_can_isolate_letters
+    board = Board.new
+
+    assert_equal [65, 66, 67], board.isolate_letters(["A1", "B2", "C3"])
+  end
+
 
   def test_placement_is_consecutive
     skip
@@ -54,12 +88,5 @@ class BoardTest < Minitest::Test
     assert_equal false, board.valid_placement?(submarine, ["A1", "C1"])
     assert_equal false, board.valid_placement?(cruiser, ["A3", "A2", "A1"])
     assert_equal false, board.valid_placement?(submarine, ["C1", "B1"])
-  end
-
-  def test_create_board
-    skip
-    board = Board.new
-
-    board.create_board
   end
 end
