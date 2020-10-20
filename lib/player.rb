5f019f2c2e29fa3b95b84a02d1043ced2fc52171
Yesi Meza <yesi.meza10@gmail.com>
2020-10-20 15:16:37 -0600

    Refactored the p to puts

--- lib/player.rb
+++ lib/player.rb
@@ -12,35 +12,35 @@ class Player
   end
 
   def setup
-    p "I have laid out my ships on the grid."
-    p "You now need to lay out your two ships."
-    p "The Cruiser is three units long and the Submarine is two units long."
+    puts "I have laid out my ships on the grid."
+    puts "You now need to lay out your two ships."
+    puts "The Cruiser is three units long and the Submarine is two units long."
     puts @board.render(true)
   end
-  
+
   def place_cruiser
     loop do
-      p "Enter the squares for the Cruiser (3 spaces):"
+      puts "Enter the squares for the Cruiser (3 spaces):"
       input =  gets.chomp.upcase
       if @board.valid_placement?(@cruiser, input.split)
         @board.place(@cruiser, input.split)
         break
       else
-        p "Those are not valid Coordinates. Please try again:"
+        puts "Those are not valid Coordinates. Please try again:"
       end
     end
     puts @board.render(true)
   end
 
   def place_submarine
-    p "Enter the squares for the Submarine (2 spaces):"
+    puts "Enter the squares for the Submarine (2 spaces):"
     loop do
       input =  gets.chomp.upcase
       if @board.valid_placement?(@submarine, input.split)
         @board.place(@submarine, input.split)
         break
       else
-        p "Those are not valid coordinates. Please try again:"
+        puts "Those are not valid coordinates. Please try again:"
       end
     end
     puts @board.render(true)
