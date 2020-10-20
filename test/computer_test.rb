77070ec5df9cb779d5fc4abe2a3d53921241200a
Megan Campbell <megan.campbell26@yahoo.com>
2020-10-19 19:39:44 -0600

    Add computer class and tests

---
+++ test/computer_test.rb
@@ -0,0 +1,31 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/game'
+require './lib/player'
+require './lib/computer'
+
+class ComputerTest < Minitest::Test
+
+  def test_it_exists
+    computer = Computer.new
+
+    assert_instance_of Computer, computer
+  end
+
+  def test_it_has_ships
+    computer = Computer.new
+
+    assert_instance_of Ship, computer.cruiser
+    assert_instance_of Ship, computer.submarine
+    assert_equal 2, computer.ship_count
+  end
+
+  def test_it_can_place_cruiser
+    computer = Computer.new
+require 'pry'; binding.pry
+    computer.place_cruiser
+  end
+end
